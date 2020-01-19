8333198703d61c6d38c2d0ef6f00b04e3d27b8bb
520Matches <HengDi_Sun@163.com>
2020-01-19 15:41:33 +0800

    update hacker

--- like_netcat/bhnet.py
+++ like_netcat/bhnet.py
@@ -54,7 +54,7 @@ def main():
             listen = True
         elif o in ("-e","--execute"):
             execute = a
-        elif o in ("c","--command"):
+        elif o in ("-c","--command"):
             command = True
         elif o in ("-u","--upload"):
             upload_destination = a
